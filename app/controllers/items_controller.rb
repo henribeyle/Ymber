2bd4236ad6a25c96896b7a68cdcc315ec80ccf17
henri.beyle@gmail.com <henri.beyle@gmail.com>
2009-09-09 18:12:54 +0200

    operations: update an item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,31 +1,31 @@
 class ItemsController < ApplicationController
 #   def index
 #     @items = Item.all
-# 
+#
 #     respond_to do |format|
 #       format.html # index.html.erb
 #       format.xml  { render :xml => @items }
 #     end
 #   end
-# 
+#
 #   def show
 #     @item = Item.find(params[:id])
-# 
+#
 #     respond_to do |format|
 #       format.html # show.html.erb
 #       format.xml  { render :xml => @item }
 #     end
 #   end
-# 
+#
 #   def new
 #     @item = Item.new
-# 
+#
 #     respond_to do |format|
 #       format.html # new.html.erb
 #       format.xml  { render :xml => @item }
 #     end
 #   end
-# 
+#
 #   def edit
 #     @item = Item.find(params[:id])
 #   end
@@ -34,10 +34,10 @@ class ItemsController < ApplicationController
     @item = Item.new(params[:item])
 
     params[:tag].each do |x|
-      begin 
+      begin
         @item.tags << Tag.find(x)
       rescue ActiveRecord::RecordNotFound => e
-        render :json => { :status => 'error', :error => e.to_s } 
+        render :json => { :status => 'error', :error => e.to_s }
         return
       end
     end
@@ -45,29 +45,40 @@ class ItemsController < ApplicationController
     if @item.save then
       render :json => @item.json
     else
-      render :json => { :status => 'error', :error => @item.errors } 
+      render :json => { :status => 'error', :error => @item.errors }
     end
   end
 
-#   def update
-#     @item = Item.find(params[:id])
-# 
-#     respond_to do |format|
-#       if @item.update_attributes(params[:item])
-#         flash[:notice] = 'Item was successfully updated.'
-#         format.html { redirect_to(@item) }
-#         format.xml  { head :ok }
-#       else
-#         format.html { render :action => "edit" }
-#         format.xml  { render :xml => @item.errors, :status => :unprocessable_entity }
-#       end
-#     end
-#   end
-# 
+  def update
+    begin
+      @item = Item.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => e
+      render :json => { :status => 'error', :error => e.to_s }
+      return
+    end
+
+    if !@item.update_attributes(params[:item])
+      render :json => { :status => 'error', :error => 'could not update item' }
+      return
+    end
+
+    @item.tags.clear
+
+    params[:tag].each do |x|
+      begin
+        @item.tags << Tag.find(x)
+      rescue ActiveRecord::RecordNotFound => e
+        render :json => { :status => 'error', :error => e.to_s }
+        return
+      end
+    end
+    render :json => @item.json
+  end
+
 #   def destroy
 #     @item = Item.find(params[:id])
 #     @item.destroy
-# 
+#
 #     respond_to do |format|
 #       format.html { redirect_to(items_url) }
 #       format.xml  { head :ok }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
 #   map.resources :tags
-  map.resources :items, :only => [:create]
+  map.resources :items, :only => [:create, :update]
 
   map.connect ':value', :controller => 'tags', :action => 'editor'
   map.root :controller => "tags", :action => 'editor'
