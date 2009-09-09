6008d0ccd131c97497a4863e89a7e8145ea0f457
henri.beyle@gmail.com <henri.beyle@gmail.com>
2009-09-09 16:21:16 +0200

    model: hacer que un item no pueda tener in && next && waiting (solo uno)

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ActiveRecord::Base
   has_and_belongs_to_many :tags
 
   validates_presence_of :value
+
+  def validate
+    tg=tags.map {|t| t.value }
+    if (tg & ['in','next','waiting']).size > 1 then
+      errors.add_to_base "Tags in, next, and waiting are mutually exclusively" 
+    end
+  end
 end
