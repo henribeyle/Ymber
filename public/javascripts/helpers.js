a08247acd21fca99bfdac266754d5fe3a347fc72
henri.beyle@gmail.com <henri.beyle@gmail.com>
2009-09-18 11:22:48 +0200

    cosmetic bug error

--- public/javascripts/helpers.js
+++ public/javascripts/helpers.js
@@ -15,7 +15,7 @@ function normal_error(error) {
 }
 
 function assert_failed(what) {
-  log('assert_failed: '+what+)
+  log('assert_failed: '+what)
 }
 
 function catch_all(m,f,l) {
@@ -23,8 +23,6 @@ function catch_all(m,f,l) {
   assert_failed(m.replace(/uncaught exception: /,''))
   return true
 }
-
-
 
 function get_response(a) {
   var r=null
