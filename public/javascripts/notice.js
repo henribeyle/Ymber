9c0ada35398513f709d2c576b6917cadf902c0fa
henri.beyle@gmail.com <henri.beyle@gmail.com>
2009-09-23 10:51:34 +0200

    notices now stack on top of each other

--- public/javascripts/notice.js
+++ public/javascripts/notice.js
@@ -1,12 +1,12 @@
 ﻿(function($) {
 
    var main_opts = {
-      'position': 'fixed',
-      'display': 'none',
-      'top': '10px',
-      'right': '10px',
+      'position': 'relative',
+      'float': 'right',
+      'display': 'block',
       'border': 'none',
       'padding': '10px 10px 10px 45px',
+      'margin-bottom': '10px',
       'opacity': '0.8',
       'font-weight': 'bold',
       'color': '#fff',
@@ -20,39 +20,40 @@
       if(timeout==null)
          timeout=3000
 
-      $('<div>').attr('id','notice-ui').css({
+      var nui= $('#notice-ui')
+      if(nui.length==0)
+        nui=$('<div>').attr('id','notice-ui').appendTo('body').css({
          zIndex: 500,
          position: 'fixed',
-         display: 'none'
-      }).append(notice_node.show()).
-         appendTo('body').
-         fadeIn(500, function() {
-            setTimeout(function() {
-               notice_node.fadeOut(1000,function() {
-                  notice_node.remove()
-                  $('#notice-ui').remove()
-                  notice_node=null
-               })
-            },timeout)
-         })
+        'top': '20px',
+        'right': '20px',
+      })
+
+      var notice_wrap=$('<div>').append(notice_node)
+      nui.append(notice_wrap)
+
+      notice_wrap.fadeIn(500, function() {
+        setTimeout(function() {
+          notice_node.fadeOut(1000,function() {
+            notice_wrap.remove()
+          })
+        },timeout)
+      })
    }
 
    $.error = function(msg,timeout) {
       if(!msg) return
       var no=$.extend(main_opts, { 'background-image': 'url("/images/Red_Ball.png")' })
-      var v=$('<div>').css(no).append($('<span>').html(msg))
-      $.notice(v,timeout)
+      $.notice($('<div>').css(no).html(msg),timeout)
    }
    $.warning = function(msg,timeout) {
       if(!msg) return
       var no=$.extend(main_opts, { 'background-image': 'url("/images/Orange_Ball.png")' })
-      var v=$('<div>').css(no).append($('<span>').html(msg))
-      $.notice(v,timeout)
+      $.notice($('<div>').css(no).html(msg),timeout)
    }
    $.message = function(msg,timeout) {
       if(!msg) return
       var no=$.extend(main_opts, { 'background-image': 'url("/images/Green_Ball.png")' })
-      var v=$('<div>').css(no).append($('<span>').html(msg))
-      $.notice(v,timeout)
+      $.notice($('<div>').css(no).html(msg),timeout)
    }
 })(jQuery)
