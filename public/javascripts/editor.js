3a391419c60546ce8cd994c434ebcd9219c328c2
henri.beyle@gmail.com <henri.beyle@gmail.com>
2009-09-26 22:15:33 +0200

    support for modifying a date using the datepicker in editor
    + selection via mouse or keys

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -20,20 +20,37 @@
         self.bind('keyup', 'esc', function(){ esc && esc() })
         self.bind('keyup', 'ctrl+del', function(){ ctrldel && ctrldel() })
         self.bind('keyup', '@', at_func)
+        self.keyup(selection)
+        self.mouseup(selection)
       })
 
       self.blur(function(){
         self.unbind('keyup', 'ctrl+return')
         self.unbind('keyup', 'esc')
         self.unbind('keyup', 'ctrl+del')
+        self.unbind('keyup', '@')
+        self.unbind('keyup', selection)
+        self.unbind('mouseup', selection)
       })
 
       function at_func() {
         var self=this
-        if(self.value.substr('f@')!=-1) {
-          log('found')
+        if(self.value.indexOf('f@')!=-1) {
           $.select_date(null,function(x) { 
             self.value = self.value.replace('f@', x);
+            $(self).focus()
+          })
+        }
+      }
+
+      function selection(e) {
+        var sel=this.value.substring(this.selectionStart,this.selectionEnd)
+        var prev=this.value.substr(0,this.selectionStart);
+        var next=this.value.substr(this.selectionEnd);
+        if(/(\d{2})\/(\d{2})\/(\d{4})/.test(sel)) {
+          $.select_date(sel,function(x) { 
+            self.val(prev+x+next)
+            self.focus()
           })
         }
       }
