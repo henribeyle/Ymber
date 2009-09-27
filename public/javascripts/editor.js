833fca0b9bfee8d7ed5c9912469214e9aa9857a9
henri.beyle@gmail.com <henri.beyle@gmail.com>
2009-09-27 22:18:22 +0200

    editor disables its keys for map to take over
    still no knowledge on why the datepicker does
    not allow using keys to select dates

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -15,34 +15,41 @@
       var esc=o.esc
       var ctrldel=o.ctrldel
 
-      self.focus(function(){
+      self.focus(function(){ bind_all() })
+      self.blur(function(){ unbind_all() })
+
+      function bind_all() {
         self.bind('keyup', 'ctrl+return',function(){ ctrlenter && ctrlenter() })
         self.bind('keyup', 'esc', function(){ esc && esc() })
         self.bind('keyup', 'ctrl+del', function(){ ctrldel && ctrldel() })
         self.bind('keyup', '@', at_func)
         self.keyup(selection)
         self.mouseup(selection)
-      })
+      }
 
-      self.blur(function(){
+      function unbind_all() {
         self.unbind('keyup', 'ctrl+return')
         self.unbind('keyup', 'esc')
         self.unbind('keyup', 'ctrl+del')
         self.unbind('keyup', '@')
         self.unbind('keyup', selection)
         self.unbind('mouseup', selection)
-      })
+      }
 
       function at_func() {
         var self=this
         if(self.value.indexOf('f@')!=-1) {
+          unbind_all()
           $.select_date(null,function(x) {
+            bind_all()
             self.value = self.value.replace('f@', x);
             $(self).focus()
           })
         }
         if(self.value.indexOf('m@')!=-1) {
+          unbind_all()
           $.map_show(function(lat,lng) {
+            bind_all()
             if(!lat) return
             self.value = self.value.replace('m@', '{'+lat+','+lng+'}');
             $(self).focus()
@@ -55,7 +62,9 @@
         var prev=this.value.substr(0,this.selectionStart);
         var next=this.value.substr(this.selectionEnd);
         if(/\d{2}\/\d{2}\/\d{4}/.test(sel)) {
+          unbind_all()
           $.select_date(sel,function(x) {
+            bind_all()
             self.val(prev+x+next)
             self.focus()
           })
@@ -63,7 +72,9 @@
         var mp=/\{(\d+\.\d+),(\d+\.\d+)\}/
         if(mp.test(sel)) {
           var m=mp.exec(sel)
+          unbind_all()
           $.map_show(function(t,g) {
+            bind_all()
             if(!t) return
             self.val(prev+'{'+t+','+g+'}'+next)
             self.focus()
