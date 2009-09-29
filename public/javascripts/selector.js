e94798014690e86366c4e4b91ddae72dd534c0ad
henri.beyle@gmail.com <henri.beyle@gmail.com>
2009-09-29 20:24:51 +0200

    moving modal into selector (3rd step)

--- public/javascripts/selector.js
+++ public/javascripts/selector.js
@@ -1,17 +1,9 @@
 (function($) {
-  var empty_input='type here ...'
   var filtered=[]
   var values=[]
   var selected=null
   var content=''
   var fire=true
-
-  var destroy = function() {
-    if($('#selector-ui').length != 0) {
-      $(document).unbind('keypress', keys)
-      $('#selector-ui').remove()
-    }
-  }
 
   var change = function() {
     var colored = function(color,text) {
@@ -23,35 +15,30 @@
       if(nx!=x) return nx
       return null
     })
-    $('#selector-ui-input').html(content=='' ? empty_input : content)
+    $('#selector-ui-input').html(content=='' ? 'type here ...' : content)
     if(filtered.length > 0)
       $('#selector-ui-values').html(filtered.join(' '))
     else
       $('#selector-ui-values').html(colored('#F7803C','no matches'))
   }
 
-  function onclose() {
-    destroy()
-    fire && selected && selected()
-  }
+  function modal_hide() {
+    if($('#selector-ui').length == 0) {
+      $.error('modal hide on no elements')
+      return
+    }
 
-  $.modal_hide = function() {
-    if($('#selector-ui').length == 0) return
     $(document).unbind('mousedown mouseup keydown keypress', modal_handler)
+    $(document).unbind('keypress', keys)
     $('#selector-ui-overlay,#selector-ui-wrapper').remove()
-    onclose()
+
+    fire && selected && selected()
   }
 
   function modal_handler(e) {
     return $(e.target).
       parents('#selector-ui-overlay,#selector-ui-wrapper').
       length > 0
-  }
-
-  var init = function() {
-    filtered=[]
-    content=''
-    fire=true
   }
 
   var keys = function(e) {
@@ -67,16 +54,14 @@
     }
     if(e.which == 13) {
       if(filtered.length == 1 ) {
-        destroy()
         fire=false
-        $.modal_hide()
+        modal_hide()
         selected && selected(filtered[0].replace(/<\/?[^>]+>/gi, ''))
       } else {
         var pos=$.inArray(content, values)
         if(pos!=-1) {
-          destroy()
           fire=false
-          $.modal_hide()
+          modal_hide()
           selected && selected(values[pos])
         }
       }
@@ -90,12 +75,20 @@
   }
 
   $.selector = function(title,values2,selected2) {
+    if($('#selector-ui').length != 0) {
+      $.error('cant have more than one selector')
+      return
+    }
+
     values=values2
     selected=selected2
 
-    destroy()
-    init()
-    $.modal_hide()
+    filtered=[]
+    content=''
+    fire=true
+
+    $('<div>').attr('id','selector-ui-overlay').
+      attr('title','Click to close').click(modal_hide).appendTo('body')
 
     var selector_div=$('<div>').attr('id','selector-ui')
     $('<div>').attr('id','selector-ui-title').html(title).appendTo(selector_div)
@@ -103,8 +96,6 @@
     $('<hr>').appendTo(selector_div)
     $('<div>').attr('id','selector-ui-values').appendTo(selector_div)
 
-    $('<div>').attr('id','selector-ui-overlay').
-      attr('title','Click to close').click($.modal_hide).appendTo('body')
 
     $('<div>').attr('id','selector-ui-wrapper').append(selector_div).appendTo('body')
 
@@ -113,7 +104,7 @@
     change()
 
     $(document).bind('mousedown mouseup keydown keypress', modal_handler).
-      bind('keyup', 'esc', $.modal_hide )
+      bind('keyup', 'esc', modal_hide )
 
     $(document).bind('keypress', keys)
   }
