e90b7edef42da26b54e9b4758f42a9cfd6e98743
henri.beyle@gmail.com <henri.beyle@gmail.com>
2009-10-09 23:15:23 +0200

    first pass at Item class

--- lib/git.rb
+++ lib/git.rb
@@ -1,56 +1,69 @@
 DBDIR=File.dirname(__FILE__) + "/../db/gitdb"
 
-def git(*command)
-  gd="--git-dir #{DBDIR}/.git"
-  wt="--work-tree #{DBDIR}"
-  return %x{git #{gd} #{wt} #{command.join(' ')}}
-end
+module Utils
 
-def list(glob)
-  Dir.chdir(DBDIR) { return Dir.glob(glob) }
-end
-
-def next_id(name)
-  a=list(name+'_*').map { |x| x.gsub(/^#{name}/,'').to_i }.sort
-  return a.length==0 ? 1 : a.last+1
-end
-
-def next_item_id
-  return next_id('item')
-end
-
-def next_tag_id
-  return [next_id('tag_value'),next_id('tag_extra')].max
-end
-
-def write_to(file,*value)
-  File.open(File.join(DBDIR,file), "w") do |f|
-    f.print value.join('')
+  def Utils.git(*command)
+    gd="--git-dir #{DBDIR}/.git"
+    wt="--work-tree #{DBDIR}"
+    return %x{git #{gd} #{wt} #{command.join(' ')}}
   end
-  git('add',file)
-end
 
-def read_from(file)
-  return IO.read(File.join(DBDIR,file))
-end
+  def Utils.list(glob)
+    Dir.chdir(DBDIR) { return Dir.glob(glob) }
+  end
 
-def remove(file)
-  git('rm',file)
-#   File.unlink(File.join(DBDIR,file))
-end
+  def Utils.next_id(name)
+    a=list(name+'_*').map { |x| x.gsub(/^#{name}/,'').to_i }.sort
+    return a.length==0 ? 1 : a.last+1
+  end
 
-def item_new(value)
-  id=next_item_id
-  write_to("item_#{id}",value)
-  return id
-end
+  def Utils.next_item_id
+    return next_id('item')
+  end
 
-def tag_new(value,extra=nil)
-  id=next_tag_id
-  write_to("tag_value_#{id}",value)
-  write_to("tag_extra_#{id}",extra) if !extra.nil? and extra != ''
-  return id
-end
+  def Utils.next_tag_id
+    return [next_id('tag_value'),next_id('tag_extra')].max
+  end
+
+  def Utils.write_to(file,*value)
+    File.open(File.join(DBDIR,file), "w") do |f|
+      f.print value.join('')
+    end
+    git('add',file)
+  end
+
+  def Utils.read_from(file)
+    return IO.read(File.join(DBDIR,file))
+  end
+
+  def Utils.remove(file)
+    git('rm',file)
+  end
+
+  def Utils.item_new(value)
+    id=next_item_id
+    write_to("item_#{id}",value)
+    return id
+  end
+
+  def Utils.tag_new(value,extra=nil)
+    id=next_tag_id
+    write_to("tag_value_#{id}",value)
+    write_to("tag_extra_#{id}",extra) if !extra.nil? and extra != ''
+    return id
+  end
+
+  def Utils.item_tag_new(item_id,tag_id)
+    write_to("#{item_id}@#{tag_id}")
+  end
+
+  def Utils.item_tag_destroy(item_id,tag_id)
+    remove("#{item_id}@#{tag_id}")
+  end
+
+  def Utils.save
+    git('commit','-m','x')
+  end
 
 # puts git('status')
 
@@ -65,6 +78,40 @@ end
 # write_to('5@10')
 # remove('5@10')
 
-p item_new('item')
-p tag_new('tag1')
-p tag_new('tag2','extra2')
+# p item_new('item')
+# p tag_new('tag1')
+# p tag_new('tag2','extra2')
+
+# i=item_new('item')
+# t=tag_new('tag1')
+# item_tag_new(i,t)
+# p list('*')
+# save
+# item_tag_destroy(i,t)
+# save
+end
+
+class Item
+  def initialize(value,id=nil)
+    @id=id
+    @value=value
+  end
+
+  def save
+    @id=Utils.item_new(@value)
+    #validations here!!
+    Utils.save
+  end
+
+  def Item.find(id)
+    a=Utils.list("item_#{id}")
+    raise "Item id='#{id}' not found" if a.length != 1
+    return Item.new(Utils.read_from("item_#{id}"),id)
+  end
+end
+
+# i=Item.new('hola')
+# i.save()
+
+i=Item.find(1)
+p i
