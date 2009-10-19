bbcb42061cd0e4545d2f23b442968994aec2f635
henri.beyle@gmail.com <henri.beyle@gmail.com>
2009-10-19 17:58:19 +0200

    accepting shift + ctrl + alt in main commands

--- public/javascripts/input_handling.js
+++ public/javascripts/input_handling.js
@@ -43,176 +43,143 @@ function enter(e) { return e.which==13 }
 
 var command=''
 function main_key_handler(e) {
-//log('[main]? '+e.which)
+// log('[main]? '+e.which)
   if(!my_event(e)) return
-//log('[main] '+e.which)
-  if(e.ctrlKey || e.altKey || e.metaKey || e.which == 8) {
+// log('[main] '+e.which)
+  if(e.which == 8) {
     command=''
     return true
   }
+  if(e.which == 16 || e.which == 17 || e.which == 18) return true
 
   var is=_d.item_show
   var c=String.fromCharCode(e.which)
+  c = e.shiftKey ? c.toUpperCase(c) : c.toLowerCase(c)
+  c = e.ctrlKey ? 'ctrl+'+c : c
+  c = e.altKey ? 'alt+'+c : c
   command+=c
-  //log("value :'"+c+"' = "+e.which+" command:'"+command+"'")
+// log("value :'"+c+"' = "+e.which+" command:'"+command+"'")
+
+  clear_command=true
   switch(command) {
-    case 'J': item_show_next() ; command='' ; break
-    case 'K': item_show_prev() ; command='' ; break
-
-    case 'H': item_move_selection_down() ; command='' ; break
-    case 'L': item_move_selection_up() ; command='' ; break
-
-    case 'NI':
-      add_item_helper()
-      command=''
+    case 'gi': go_to('in') ; break
+    case 'gn': go_to('next') ; break
+    case 'gw': go_to('waiting') ; break
+    case 'gt':
+      var tags=_d.tags.grep(not_main_tag).map(get_value)
+      tags.push('(none)')
+      $.selector('Select a tag to go to',tags,function(tagv) {
+        if(tagv)
+          go_to(tagv!='(none)' ? tagv : '/')
+      })
       break
 
-    case 'NT':
-      add_tag_helper()
-      command=''
-      break
+    case 'j': item_show_next() ; break
+    case 'k': item_show_prev() ; break
+    case 'J': item_move_selection_down() ; break
+    case 'K': item_move_selection_up() ; break
 
-    case 'E':
-      if(is!=null)
-        is.edit()
-      command=''
-      break
+    case '9': item_collapse_all() ; break
+    case '0': item_expand_all() ; break
 
-    case 'O':
-      if(is!=null)
-        item_toggle_expand(is)
-      command=''
-      break
+    case 'n': add_item_helper() ;  break
+    case 'e': is && is.edit() ; break
+    case 'o': is && item_toggle_expand(is) ; break
 
-    case '9':
-      item_expand_all()
-      command=''
-      break
-
-    case '0':
-      item_collapse_all()
-      command=''
-      break
-
-    case 'Y':
-      if(is!=null && _d.main_tag.value=='in')
-        item_send_to_next(is,p_mess('item sent to next'))
-      command=''
-      break
-
-    case 'W':
-      if(is!=null && _d.main_tag.value=='in')
-        item_send_to_waiting(is,p_mess('item sent to waiting'))
-      command=''
-      break
-
-    case 'A':
+    case 'a':
       if(is!=null) {
         var tags=_d.tags.grep(not_has_tag(is)).map(get_value)
         $.selector('Select a tag to add',tags,function(tagv) {
-          //log(tagv?'tag:'+tagv:'none')
           if(tagv)
             item_add_tag(is,_d.tag_value(tagv),
               p_mess('tag added to item'))
         })
       }
-      command=''
       break
 
-    case 'R':
+    case 'r':
       if(is!=null) {
         var tags=is.tags.map(get_value)
         $.selector('Select a tag to remove',tags,function(tagv) {
-          //log(tagv?'tag:'+tagv:'none')
           if(tagv)
             item_remove_tag(is,_d.tag_value(tagv),
               p_mess('tag removed from item'))
         })
       }
-      command=''
       break
 
-    case 'TE':
+    case 'tn': add_tag_helper() ; break
+
+    case 'te':
       var tags=_d.tags.map(get_value)
       $.selector('Select a tag to edit',tags,function(tagv) {
-        //log(tagv?'tag:'+tagv:'none')
         if(tagv)
           _d.tag_value(tagv).edit()
       })
-      command=''
       break
 
-    case 'FT':
+    case 'ft':
       var tags=_d.tags.grep(not_a_filter).map(get_value)
       $.selector('Select a tag to filter by',tags,function(tagv) {
-        //log(tagv?'tag:'+tagv:'none')
         if(tagv)
           tag_filter(_d.tag_value(tagv))
       })
-      command=''
       break
 
-    case 'FR':
+    case 'fr':
       var tags=_d.filters.map(get_value)
       $.selector('Select a tag to unfilter',tags,function(tagv) {
-        //log(tagv?'tag:'+tagv:'none')
         if(tagv)
           tag_unfilter(_d.tag_value(tagv))
       })
-      command=''
       break
 
-    case 'FC':
+    case 'fc':
       _d.tags.concat().each(tag_unfilter)
-      command=''
       break
 
-    case 'FO':
-      or_filtering()
-      command=''
-      break
-    case 'FA':
-      and_filtering()
-      command=''
-      break
+    case 'fo': or_filtering() ; break
+    case 'fa': and_filtering() ; break
 
-    case 'GT':
-      var tags=_d.tags.grep(not_main_tag).map(get_value)
-      tags.push('(none)')
-      $.selector('Select a tag to go to',tags,function(tagv) {
-        //log(tagv?'tag:'+tagv:'none')
-        if(tagv)
-          go_to(tagv!='(none)' ? tagv : '/')
-      })
-      command=''
-      break
+    case 'c': $.calendar_show() ; break
 
-    case 'C':
-      $.calendar_show()
-      command=''
-      break
-
-    case 'UU':
+    case 'uu':
       undo()
-      command=''
       break
 
-    case 'UR':
+    case 'ur':
       redo()
-      command=''
       break
 
-    case 'S':
+    case 'ctrl+¾':
       var sel=window.getSelection().
         toString().
         replace(/\n#/g,"\n -").
         replace(/^# /,'')
       if(is!=null && is.value.test(sel.escapeRegExp()))
         item_split(is,sel)
-      command=''
       break;
+
+    case 'y':
+      is &&
+      this_tag=='in' &&
+      item_send_to_next(is,p_mess('item sent to next'))
+      break
+
+    case 'w':
+      is &&
+      this_tag=='in' &&
+      item_send_to_waiting(is,p_mess('item sent to waiting'))
+      break
+
+    default:
+      clear_command=false
+      break
   }
 
-  setTimeout(function(){ command='' }, 300)
+  if(clear_command)
+    command=''
+  else
+    setTimeout(function(){ command='' }, 300)
   return false
 }
